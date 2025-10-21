230cd534e20ae6e565a2c126c71e204586a6ed74
Twu1738 <terrencewu1738@gmail.com>
2025-10-22 10:15:16 +1300

    Expiremented mainly with maps and classes in javascript
    Also did some experimentation with containers to format everything on the page

--- javascript.js
+++ javascript.js
@@ -1,15 +1,50 @@
-let amountPressed = 0
+let numOfClicks = 0
+let clickPower = 1
+let idleClicks = 0
 
-function addOne() {
-    amountPressed += 1;
-
-    document.getElementById("counter").textContent = amountPressed
+function updateClicks() {
+    document.getElementById("counter").textContent = numOfClicks
 }
 
-function upgrade(required) {
-    if (amountPressed >= required) {
-        amountPressed -= required;
+function addClicks() {
+    numOfClicks += clickPower
+    updateClicks()
+}
+
+
+class Upgrades {
+    /**
+        Includes: 
+        - upgrade effects
+        - upgrade validator and upgrader
+     */
+
+    upgradeEffects = {
+        // map of the upgrade effects and arrow function for the effect
+        add: (x, amount) => x + amount,
+        multiply: (x, amount) => x * amount,
     }
 
-    document.getElementById("counter").textContent = amountPressed
+    upgrade(required, which, increaseType, amount) {
+        /**
+            * @param {*} required - the number of clicks needed for the upgarde
+            * @param {*} which - the type of upgrade (e.g. CPS or Clicker Button)
+            * @param {*} increaseType - the 
+            * @param {*} amount - the amount increased
+         */
+        if (numOfClicks >= required) {
+            numOfClicks -= required;
+            
+            this.upgradeEffects.increaseType(which, amount)
+
+            updateClicks()
+        }
+    }
 }
+
+function clicksPerSec(cps) {
+    numOfClicks += cps   
+}
+
+
+
