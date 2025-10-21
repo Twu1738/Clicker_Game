9d627c4b9cc23c26df6ddec9e6e75eb765988a9e
Twu1738 <terrencewu1738@gmail.com>
2025-10-22 10:49:12 +1300

    Experimenting further with classes and methods
    Current not working, the upgrade buttons aren't upgrading

--- javascript.js
+++ javascript.js
@@ -12,12 +12,25 @@ function addClicks() {
 }
 
 
-class Upgrades {
+class upgrades {
     /**
         Includes: 
         - upgrade effects
         - upgrade validator and upgrader
      */
+
+    constructor(required, which, increaseType, amount) {
+        /**
+            * @param {*} required - the number of clicks needed for the upgarde
+            * @param {*} which - the type of upgrade (e.g. CPS or Clicker Button)
+            * @param {*} increaseType - the 
+            * @param {*} amount - the amount increased
+         */
+        this.required = required
+        this.which = which
+        this.increaseType = increaseType
+        this.amount = amount
+    }
 
     upgradeEffects = {
         // map of the upgrade effects and arrow function for the effect
@@ -25,25 +38,29 @@ class Upgrades {
         multiply: (x, amount) => x * amount,
     }
 
-    upgrade(required, which, increaseType, amount) {
-        /**
-            * @param {*} required - the number of clicks needed for the upgarde
-            * @param {*} which - the type of upgrade (e.g. CPS or Clicker Button)
-            * @param {*} increaseType - the 
-            * @param {*} amount - the amount increased
-         */
+    upgradeChecker() {
         if (numOfClicks >= required) {
             numOfClicks -= required;
-            
-            this.upgradeEffects.increaseType(which, amount)
 
-            updateClicks()
+            const which = super(which);
+            const amount = super(amount);
+            
+            this.upgradeEffects.increaseType(which, amount);
+
+            updateClicks();
         }
     }
 }
 
+class display extends upgrades{
+    // extend basically passes the construct function of the 
+    // upgrade class to this class and then use those values to display stuff
+    // idea: could instead do class upgrades extends display and then have clickpower and etc 
+    // as part of the constructor in the display class
+}
+
 function clicksPerSec(cps) {
-    numOfClicks += cps   
+    numOfClicks += cps;   
 }
 
 
