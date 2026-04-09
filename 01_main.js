ce30aa16d7d2dc9feb36c4c1e5c85ed0909ec35d
Terrence Wu <terrencewu1738@gmail.com>
2026-04-09 16:20:10 +1200

    Buttons appended in JS instead

--- 01_main.js
+++ 01_main.js
@@ -2,25 +2,19 @@
 import {gameState, upgrades} from './02_gameState.js';
 
 // imports for functions
-import {updateClicks, addClicks, createUpgradeButton, buyUpgrade} from './03_functions.js';
+import {updateClicks, addClicks, appendUpgrades, createUpgradeButtons, buyUpgrade} from './03_functions.js';
 
 // main here
 // main clicker button
 const mainClicker = document.getElementById('main-clicker');
 mainClicker.addEventListener('click', addClicks);
 
-// fetches the upgrade button container
-const upgradeContainer = document.getElementById('upgrade-container');
-
 // creates the upgrade buttons
-const addOneButton = createUpgradeButton(upgrades[0], 'cost-1' )
-const multiplyOne = createUpgradeButton(upgrades[1], 'cost-2')
-const idleOne = createUpgradeButton(upgrades[2], 'cost-3')
+createUpgradeButtons(upgrades[0], 'cost-1' )
+createUpgradeButtons(upgrades[1], 'cost-2')
+createUpgradeButtons(upgrades[2], 'cost-3')
 
-// appends the upgrade buttons to the upgrade container
-upgradeContainer.appendChild(addOneButton)
-upgradeContainer.appendChild(multiplyOne)
-upgradeContainer.appendChild(idleOne)
+
 
 // every second it adds the idle click power to counter
 setInterval(() => {gameState.numOfClicks += gameState.idleClickPower; 
--- 03_functions.js
+++ 03_functions.js
@@ -1,6 +1,9 @@
 // imports for game data 
 import {gameState, upgrades} from './02_gameState.js';
 
+// fetches the upgrade button container
+const upgradeContainer = document.getElementById('upgrade-container');
+
 // updates the click counter
 export function updateClicks() {
     document.getElementById("counter").textContent = gameState.numOfClicks;
@@ -12,17 +15,28 @@ export function addClicks() {
     updateClicks();
 }
 
+// appends the upgrades to the upgrade container
+export function appendUpgrades (button) {
+    const buttonToAppend = button
+    upgradeContainer.appendChild(buttonToAppend)
+}
+
 // creates the upgrade buttons
-export function createUpgradeButton (upgrade, costId) {
+export function createUpgradeButtons (upgrade, costId) {
     const button = document.createElement('button');
+
+    // adds the button characteristics
     button.className = 'test-button';
     button.innerHTML = `${upgrade.name} Cost: <span id="${costId}">${upgrade.cost}</span>`
     button.addEventListener('click', () => buyUpgrade(upgrade.id, costId))
-    return button
+    
+    // appends to upgrade container
+    appendUpgrades(button)
 }
 
 // checks user has enough clicks for upgrade and updates cost/level
 export function buyUpgrade(upgradeId, upgradeCostId) {
+    // finds the corresponding upgrade usign the ID 
     const upgrade = upgrades.find(item => item.id === upgradeId)
 
     if (gameState.numOfClicks >= upgrade.cost) {
