132f2da1cebb494a783a364c58c3e53edd8cde81
Terrence Wu <terrencewu1738@gmail.com>
2026-04-13 15:16:47 +1200

    Added basic saving system
    I disabled it for now so I can still perform testing and see the effects quickly

--- 01_main.js
+++ 01_main.js
@@ -15,3 +15,24 @@ renderUpgrades()
 // every second it adds the idle click power to counter
 setInterval(() => {gameState.numOfClicks += gameState.idleClickPower; 
     updateClicks()}, 1000)
+
+// // saving
+// setInterval( () => {
+//     localStorage.setItem("gameState", JSON.stringify(gameState));
+//     localStorage.setItem("upgrades", JSON.stringify(upgrades))
+// }, 5000)
+
+// loading
+// const savedGameState = JSON.parse(localStorage.getItem("gameState"));
+// const savedUpgrades = JSON.parse(localStorage.getItem("upgrades"));
+
+// if (savedGameState) {
+//     gameState.numOfClicks = savedGameState.numOfClicks
+//     gameState.clickPowerBase = savedGameState.clickPowerBase
+//     gameState.clickPowerMultiplier = savedGameState.clickPowerMultiplier
+//     gameState.idleClickPower = savedGameState.idleClickPower
+// }
+
+// if (savedUpgrades) {
+//     upgrades = savedUpgrades
+// }
